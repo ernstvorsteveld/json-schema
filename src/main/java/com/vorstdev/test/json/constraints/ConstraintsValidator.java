75c1d9e07019c36b0c64e3b73b4db0180852856f
Ernst Vorsteveld <ernst.vorsteveld@iwelcome.com>
2019-08-07 09:20:08 +0300

    added validator stream

--- src/main/java/com/vorstdev/test/json/constraints/ConstraintsValidator.java
+++ src/main/java/com/vorstdev/test/json/constraints/ConstraintsValidator.java
@@ -6,7 +6,9 @@ import com.vorstdev.test.json.JsonValidator;
 import com.vorstdev.test.json.schema.SchemaDefinition;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ConstraintsValidator extends AbstractValidator implements JsonValidator {
 
@@ -20,7 +22,13 @@ public class ConstraintsValidator extends AbstractValidator implements JsonValid
     @Override
     public boolean validate(String userFile) {
         Map<String, Object> userMap = getUserMap(userFile);
-        return true;
+        List<Constraint> failed = this.constraints.getConstraints().stream().filter(c -> validate(userMap, c))
+                .collect(Collectors.toList());
+        return failed.isEmpty();
+    }
+
+    private boolean validate(Map<String, Object> userMap, Constraint c) {
+        return false;
     }
 
     private Map<String, Object> getUserMap(String userFile) {
